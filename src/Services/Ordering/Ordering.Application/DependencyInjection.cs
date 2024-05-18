03886084dd8e2c4c2fd6f666c01eaa04abb77382
djquangsumi@gmail.com <djquangsumi@gmail.com>
2024-05-18 22:49:19 +0700

    ordering api DDD and clean architecture

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.Behaviors;
+using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Ordering.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(assembly);
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        //services.AddFeatureManagement();
+        //services.AddMessageBroker(configuration, assembly);
+
+        return services;
+    }
+}
